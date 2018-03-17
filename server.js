ce0c19267372aacbbf1ef71f938c8f0a2f8982d3
mvargasvega <martin@digitalnest.org>
2018-03-16 20:27:53 -0700

    Created the routes to edit, delete, and update students

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
 var expressLayouts = require('express-ejs-layouts');
 
 
@@ -13,7 +14,7 @@ var db = require("./models"),
 var app = express();
 
 app.use(expressLayouts);
-
+app.use(bodyParser.urlencoded({ extended: true}));
 //Load View Engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -40,13 +41,76 @@ app.get('/login', function(req, res) {
   res.render('login');
 });
 
+//create a student
+app.post('/api/students', function(req, res){
+//this is model that both creates and saves the student model
+  Student.create({
+    FirstName: req.body.FirstName,
+    LastName: req.body.LastName,
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
+  },function (err, student){//this will give us an error if anything goes wrong
+    if(err){
+      res.status(500);// response for an error
+    }
+    res.status(200).json(student);//response for success
+  });
+});
+
+app.put('/api/students/:id', function(req, res){
+  console.log('i am here');
+  var studentId = req.params.id;
+  Student.findOne({_id: studentId},function (err, currentStudent){
+    if(err){
+      res.status(500).json("user doesnt exist");
+    }
+    console.log(currentStudent);
+    currentStudent.FirstName = req.body.FirstName || currentStudent.FirstName;
+    currentStudent.LastName = req.body.LastName || currentStudent.LastName;
+    currentStudent.email = req.body.email || currentStudent.email;
+    currentStudent.password = req.body.password || currentStudent.password;
+    currentStudent.role = req.body.role || currentStudent.role;
+    console.log(currentStudent);
+    currentStudent.save(function(err,updated){
+      if(err){
+        res.status(500).json("user cant be updated");
+        throw err;
+      }
+      console.log(updated);
+      res.status(200).json(updated);
+    });
+  });
+});
+
+app.delete('/api/students/:id', function(req, res){
+  console.log('student deleted', req.params);
+  var studentId = req.params.id;
+  Student.findOneAndRemove({_id: studentId},function (err, deletedStudent){
+    if(err){
+      res.status(500);
+    }
+      res.status(200).json(deletedStudent);
+  });
+});
+
+
 // API ROUTES
+//get all students
 app.get('/api/students', function (req, res) {
   console.log("I work")
   Student.find()
   .exec(function(err, allStudents){
     if (err) {return console.log("index error:" + err); }
     res.json(allStudents);
+  });
+});
+
+// one students
+app.get('/api/students/:id', function (req, res){
+  console.log("one student");
+  Student.findOne({_id: req.params.id }, function(err, data){
+    res.json(data);
   });
 });
 
