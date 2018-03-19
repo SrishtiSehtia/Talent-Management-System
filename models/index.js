d6d50181b88a49ff079cbba28d66ba5b2e7a7372
Srishti Sehtia <srishti0123@gmail.com>
2018-03-19 10:59:30 -0700

    update mongoose connection

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/talent_management_system");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/talent_management_system");
 console.log(mongoose.connection.readyState);
 
 module.exports.Student = require("./student");
